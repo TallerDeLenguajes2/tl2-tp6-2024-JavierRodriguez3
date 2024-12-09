d9232da329b8761cdec427232169717e17ef887e
JavierRodriguez3 <javrodriguez2003@gmail.com>
2024-12-09 20:56:29 -0300

    save

--- obj/Debug/net8.0/tl2-tp6-2024-JavierRodriguez3.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl2-tp6-2024-JavierRodriguez3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl2-tp6-2024-JavierRodriguez3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeb77f5e078393fa0e61b16e5fad3827db8edf3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcde0670790939e9cb31e5e0adc1b74d40fd33a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl2-tp6-2024-JavierRodriguez3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl2-tp6-2024-JavierRodriguez3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
